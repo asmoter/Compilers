3349364703e2d41de2ef37486feedec6abe00a6c
Aleksandra Smoter <a.m.smoter@gmail.com>
2020-03-30 20:37:41 +0200

    Lab 2

---
+++ Lab1/scanner.py
@@ -0,0 +1,82 @@
+import sys
+
+import ply.lex as lex
+
+reserved = {
+    'if': 'IF',
+    'then': 'THEN',
+    'else': 'ELSE',
+    'while': 'WHILE',
+    'for': 'FOR',
+    'break': 'BREAK',
+    'continue': 'CONTINUE',
+    'return': 'RETURN',
+    'eye': 'EYE',
+    'zeros': 'ZEROS',
+    'ones': 'ONES',
+    'print': 'PRINT'
+}
+
+tokens = ['DOTADD', 'DOTSUB', 'DOTMUL', 'DOTDIV',
+          'ADDASIGN', 'SUBASSIGN', 'MULASSIGN', 'DIVASSIGN',
+          'LESSEQUAL', 'MOREEQUAL', 'NOTEQUAL', 'EQUAL',
+          'ID', 'FLOAT', 'INTNUM', 'STRING'] + list(reserved.values())
+
+literals = ['+', '-', '*', '=', ':', ';', ',', '\'', '/', '>', '<', '(', ')', '[', ']', '{', '}']
+
+t_DOTADD = r'\.\+'
+t_DOTSUB = r'\.\-'
+t_DOTMUL = r'\.\*'
+t_DOTDIV = r'\.\/'
+t_ADDASIGN = r'\+='
+t_SUBASSIGN = r'\-='
+t_MULASSIGN = r'\*='
+t_DIVASSIGN = r'/='
+t_LESSEQUAL = r'<='
+t_MOREEQUAL = r'>='
+t_NOTEQUAL = r'!='
+t_EQUAL = r'=='
+t_ignore = '  \t'
+t_ignore_COMMENT = r'\#.*'
+
+
+def t_ID(t):
+    r'[a-zA-Z_]\w*'
+    t.type = reserved.get(t.value, 'ID')
+    return t
+
+
+def t_FLOAT(t):
+    r'([0-9]*\.[0-9]+([E][-]*[1-9])*|[0-9]+\.[0-9]*([E][-]*[1-9])*)'
+    t.value = float(t.value)
+    return t
+
+
+def t_INTNUM(t):
+    r'[1-9][0-9]*'
+    t.value = int(t.value)
+    return t
+
+
+def t_STRING(t):
+    r'("[a-zA-Z ]*")'
+    return t
+
+
+def t_newline(t):  # obsluga numerow linii
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+
+def t_error(t):
+    print("line %d: Illegal character '%s'" % (token.lineno, t.value[0]))
+    t.lexer.skip(1)
+
+
+lexer = lex.lex()
+fh = open(sys.argv[1], "r")
+lexer.input(fh.read())
+for token in lexer:
+    print("line %d: %s(%s)" % (token.lineno, token.type, token.value))
+
+
---
+++ Lab2/parser.py
@@ -0,0 +1,199 @@
+#!/usr/bin/python
+
+import scanner
+import ply.yacc as yacc
+
+tokens = scanner.tokens
+
+precedence = (
+    ("nonassoc", "IFX"),
+    ("nonassoc", "ELSE"),
+    ("nonassoc", "=", "ADDASSIGN", "SUBASSIGN", "MULASSIGN", "DIVASSIGN"),
+    ("nonassoc", "EQUAL", "NOTEQUAL"),
+    ("nonassoc", "<", ">", "LESSEQUAL", "MOREEQUAL"),
+    ("left", '+', '-', "DOTADD", "DOTSUB"),
+    ("left", '*', '/', 'DOTMUL', 'DOTDIV'),
+    ("right", "UNEGATION"),
+    ("left", "TRANSPOSE")
+)
+
+
+def p_error(p):
+    if p:
+        print("Syntax error at line {0}, LexToken({1}, '{2}')".format(p.lineno, p.type, p.value))
+    else:
+        print("Unexpected end of input")
+
+
+def p_program(p):
+    """program : instructions_opt"""
+
+
+def p_instructions_opt_1(p):
+    """instructions_opt : instructions """
+
+
+def p_instructions_opt_2(p):
+    """instructions_opt : """
+
+
+def p_instructions_1(p):
+    """instructions : instructions instruction """
+
+
+def p_instructions_2(p):
+    """instructions : instruction """
+
+
+def p_instruction(p):
+    """instruction : block
+                   | if
+                   | print
+                   | while
+                   | for
+                   | break
+                   | continue
+                   | return
+                   | assign
+                   | assign_in_array """
+
+
+def p_block(p):
+    """ block : '{' instructions '}' """
+
+
+def p_if(p):
+    """ if : IF '(' assignment ')' instruction %prec IFX
+           | IF '(' assignment ')' instruction ELSE instruction """
+
+
+def p_print(p):
+    """ print : PRINT arguments ';' """
+
+
+def p_arguments(p):
+    """ arguments : assignment
+                  | assignment ',' arguments """
+
+
+def p_while(p):
+    """ while : WHILE '(' assignment ')' instruction """
+
+
+def p_for(p):
+    """ for : FOR ID '=' range instruction """
+
+
+def p_range(p):
+    """ range : expression ':' expression """
+
+
+def p_break(p):
+    """ break : BREAK ';' """
+
+
+def p_continue(p):
+    """ continue : CONTINUE ';' """
+
+
+def p_return(p):
+    """ return : RETURN ';'
+               | RETURN assignment ';' """
+
+
+def p_assign(p):
+    """ assign : ID '=' assign
+               | ID '=' assignment ';'
+               | ID ADDASSIGN assignment ';'
+               | ID SUBASSIGN assignment ';'
+               | ID MULASSIGN assignment ';'
+               | ID DIVASSIGN assignment ';' """
+
+
+def p_assign_in_array(p):
+    """ assign_in_array : ID array_range '=' assign
+                        | ID array_range '=' assignment ';'
+                        | ID array_range ADDASSIGN assignment ';'
+                        | ID array_range SUBASSIGN assignment ';'
+                        | ID array_range MULASSIGN assignment ';'
+                        | ID array_range DIVASSIGN assignment ';' """
+
+
+def p_array_range(p):
+    """ array_range : '[' INTNUM ',' INTNUM ']'
+                    | '[' INTNUM ']' """
+
+
+def p_assignment(p):
+    """ assignment : STRING
+                   | matrix_initialization
+                   | relational_expression
+                   | expression
+                   | array """
+
+
+def p_array(p):
+    """ array : '[' ']'
+              | '[' dimensions ']' """
+
+
+def p_dimensions(p):
+    """ dimensions : values
+                   | values ';' dimensions """
+
+
+def p_values(p):
+    """ values : value
+               | value ',' values """
+
+
+def p_value(p):
+    """ value : STRING
+              | number """
+
+
+def p_relational_expression(p):
+    """ relational_expression : expression '<' expression
+                              | expression '>' expression
+                              | expression LESSEQUAL expression
+                              | expression MOREEQUAL expression
+                              | expression EQUAL expression
+                              | expression NOTEQUAL expression """
+
+
+def p_expression_binary_operation(p):
+    """ expression : expression '+' expression
+                   | expression '-' expression
+                   | expression '*' expression
+                   | expression '/' expression
+                   | expression DOTADD expression
+                   | expression DOTSUB expression
+                   | expression DOTMUL expression
+                   | expression DOTDIV expression """
+
+
+def p_expression(p):
+    """ expression : number
+                   | ID """
+
+
+def p_expression_transpose(p):
+    """ expression : ID TRANSPOSE """
+
+
+def p_expression_unegation(p):
+    """ expression : '-' expression %prec UNEGATION """
+
+
+def p_matrix_initialization(p):
+    """ matrix_initialization : EYE '(' INTNUM ')'
+                                | ZEROS '(' INTNUM ')'
+                                | ONES '(' INTNUM ')' """
+
+
+def p_number(p):
+    """ number : INTNUM
+               | FLOAT """
+
+
+parser = yacc.yacc()
